da25cecceb49e351c0e83594070a26a94eeb4576
jpda <john.dandison@live.com>
2021-07-14 15:41:37 -0400

    updated config for url

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -7,42 +7,45 @@ using Microsoft.Identity.Web.Resource;
 
 namespace ProductCatalog.Controllers
 {
-  [Authorize]
-  [ApiController]
-  [Route("api/[controller]")]
-  public class CategoriesController : ControllerBase
-  {
-    SampleData data;
-
-    public CategoriesController(SampleData data)
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
     {
-      this.data = data;
-    }
+        SampleData data;
 
-    public List<Category> GetAllCategories()
-    {
-      HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Read" });
-      return data.Categories;
-    }
+        public CategoriesController(SampleData data)
+        {
+            this.data = data;
+        }
 
-    [HttpGet("{id}")]
-    public Category GetCategory(int id)
-    {
-      HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Read" });
-      return data.Categories.FirstOrDefault(p => p.Id.Equals(id));
-    }
+        public List<Category> GetAllCategories()
+        {
+            if (!HttpContext.User.IsInRole("access_as_application"))
+            {
+                HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Read" });
+            }
+            return data.Categories;
+        }
 
-    [HttpPost]
-    public ActionResult CreateCategory([FromBody] Category newCategory)
-    {
-      HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Write" });
-      if (string.IsNullOrEmpty(newCategory.Name))
-      {
-        return BadRequest("Product Name cannot be empty");
-      }
-      newCategory.Id = (data.Categories.Max(c => c.Id) + 1);
-      data.Categories.Add(newCategory);
-      return CreatedAtAction(nameof(GetCategory), new { id = newCategory.Id }, newCategory);
+        [HttpGet("{id}")]
+        public Category GetCategory(int id)
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Read" });
+            return data.Categories.FirstOrDefault(p => p.Id.Equals(id));
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory([FromBody] Category newCategory)
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(new string[] { "Category.Write" });
+            if (string.IsNullOrEmpty(newCategory.Name))
+            {
+                return BadRequest("Product Name cannot be empty");
+            }
+            newCategory.Id = (data.Categories.Max(c => c.Id) + 1);
+            data.Categories.Add(newCategory);
+            return CreatedAtAction(nameof(GetCategory), new { id = newCategory.Id }, newCategory);
+        }
     }
-  }
 }
